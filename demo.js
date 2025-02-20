b61f820409cd82ebd5387afe2643c1b664162b9d
Nandhini0267 <129576666+Nandhini0267@users.noreply.github.com>
2025-02-20 14:14:17 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,17 @@
+let a=10
+let b=30
+console.log(10+20+"10"+"Hello"+20+10)
+let subjects=["tam","eng","math","sci","soc"]
+console.log(subjects)
+let frnds=["Megu","Nandhu","Loga"]
+console.log(frnds)
+frnds.push("nandhu's hus")
+console.log(frnds)
+frnds.shift()
+console.log(frnds)
+frnds.shift()
+console.log(frnds)
+frnds.unshift("Megu")
+console.log(frnds)
+let name=prompt("name:")
+console.log("hi"+name)
